0a774ae8fd6f4723da439677e6e2fbf647b0a24e
mailcalm <285832947+mailcalm@users.noreply.github.com>
2026-05-23 00:06:59 +0100

    Fix booking link behaviour

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import FadeIn from "@/components/FadeIn";
 
@@ -339,10 +337,6 @@ target="_blank"
           </div>
         </section>
       </FadeIn>
-      <script
-  src="https://app.squareup.com/appointments/buyer/widget/poecqakuzxegtm/LFCVWV3S5QHEH.js"
-  async
-></script>
     </main>
   );
 }
