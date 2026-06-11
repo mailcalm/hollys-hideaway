e22da6f57379aeb6eab367127e24d7ce7d608ef8
mailcalm <285832947+mailcalm@users.noreply.github.com>
2026-06-11 11:11:14 +0100

    Update prices

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
         {[
           {
             title: "Builder Gel Manicure",
-            price: "£40",
+            price: "£35",
             time: "1 hour 30 mins",
             text: "A detail-focused, gentle e-file manicure with cuticle work and nail shaping. Builder gel is then carefully applied for strength. Completed with luxury hand cream, massage and nourishing oil.",
             extras: [
@@ -113,7 +113,7 @@ export default function Home() {
           },
           {
             title: "Builder Gel Rebalance Manicure",
-            price: "£45",
+            price: "£39",
             time: "1 hour 30 mins",
             text: "A maintenance treatment designed to restore strength, balance and structure as your natural nails grow. Existing builder gel is gently refined, the shape is rebalanced, and the apex is carefully rebuilt to support the nail’s natural stress points. Length can be adjusted if needed. Fresh builder gel is applied to reinforce and perfect the structure. Completed with luxury hand cream, massageand nourishing oil.",
             extras: [
@@ -124,11 +124,11 @@ export default function Home() {
           },
           {
             title: "Gel Manicure",
-            price: "£35",
+            price: "£30",
             time: "1 hour 15 mins",
             text: "A detail-focused, gentle e-file manicure with cuticle work and nail shaping. Completed with luxury hand cream, massage and nourishing oil.",
             extras: [
-              "REMOVAL + £10",
+              "REMOVAL + £8",
               "FRENCH TIP + £5",
               "MINIMAL NAIL ART + £5",
             ],
